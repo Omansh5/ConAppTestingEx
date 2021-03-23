fcd2863fdc91b09d03906818c949dfb9a6e4c93d
Omansh5 <omanshbarnwal0360@gmail.com>
2021-03-23 11:17:12 +0530

    Commit

---
+++ ConAppPlayerTesting/Player.cs
@@ -0,0 +1,11 @@
+﻿namespace ConAppPlayerTesting
+{
+    public class Player
+    {
+        public int PId { get; set; }
+
+        public string PName { get; set; }
+
+        public string PTeam { get; set; }
+    }
+}
---
+++ ConAppPlayerTesting/Program.cs
@@ -0,0 +1,31 @@
+﻿using ConAppPlayerTesting;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleAppTestPlayer
+{
+    [TestFixture]
+    class Program
+    {
+        public static List<Player> Playerlist()
+        {
+            List<Player> plylist = new List<Player>
+            {
+                new Player{PId=1,PName="Kohli",PTeam="India"},
+               new Player{PId=2,PName="Warner",PTeam="Australia"},
+               new Player{PId=3,PName="ABD",PTeam="S Africa"},
+               new Player{PId=4,PName="Guptil",PTeam="New Zealand"},
+            };
+              return plylist;
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
